d6aab60dc8b3070b79e6a02e1834dcc0c3351baa
ik012982i9 <hdiazdemendivilvi23dw@ikzubirimanteo.com>
2024-01-11 09:23:18 +0100

    ultimos cambios

--- src/operario.java
+++ src/operario.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class operario {
+public class operario extends empleado{
+    String id;
+    double salario;
+
+    public operario(String nombre, String direccion, int segSocialNum, String id, double salario){
+        super(segSocialNum,nombre,direccion);
+        this.id=id;
+        this.salario=salario;
+    }
+
+    public double getSalario(){
+        return this.salario; //reescribir
+    }
 }
